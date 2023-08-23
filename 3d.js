fea80d8aa3d07e62f518a4f3aa505ca567026841
pteacher <subfear@mail.ru>
2023-08-23 17:40:15 +0600

    fix: get it back

--- 3d.js
+++ 3d.js
@@ -7,8 +7,7 @@ let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
 
 const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setClearColor( 0x000000, 0 );
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(windowWidth, windowHeight);
 document.body.appendChild(renderer.domElement);
 
